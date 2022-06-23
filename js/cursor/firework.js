7c95979ec93ca31f76a2c10e970a5516682505e9
ruyanc <cry15@tsinghua.org.cn>
2022-06-23 20:12:38 +0800

    Site updated: 2022-06-23 20:12:38

--- js/cursor/firework.js
+++ js/cursor/firework.js
@@ -1,4 +1,4 @@
-class Circle {
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script>class Circle {
   constructor({ origin, speed, color, angle, context }) {
     this.origin = origin
     this.position = { ...this.origin }
